d79c5d3848e56219ccd845264bc35da17107a919
Kevin Brown-Silva <github@kevin-brown.com>
2025-08-16 16:23:40 -0400

    Add FTMS start/stop/pause commands

--- ftms_ble/device.py
+++ ftms_ble/device.py
@@ -1,6 +1,11 @@
 from bleak import BleakClient, BLEDevice
 from bleak_retry_connector import close_stale_connections, establish_connection
-from .const import FitnessMachineStopCode
+from .const import (
+    FTMS_FITNESS_MACHINE_CONTROL_POINT_CHARACTERISTIC_UUID,
+    FitnessMachineControlPointOperation,
+    FitnessMachineStopCode,
+)
+import struct
 
 
 class FitnessMachineDevice:
@@ -40,7 +45,9 @@ class FitnessMachineDevice:
 
         await close_stale_connections(self._device)
 
-        self._client = await establish_connection(self._device)
+        self._client = await establish_connection(
+            BleakClient, self._device, self._device.name
+        )
 
     async def disconnect(self):
         if not self.is_connected:
@@ -48,8 +55,15 @@ class FitnessMachineDevice:
 
         await self._client.disconnect()
 
+    async def request_control(self):
+        return await self._send_ftms_command(
+            FitnessMachineControlPointOperation.REQUEST_CONTROL
+        )
+
     async def start_resume(self):
-        pass
+        return await self._send_ftms_command(
+            FitnessMachineControlPointOperation.START_OR_RESUME
+        )
 
     async def stop_pause(self, stop_code: FitnessMachineStopCode):
         if stop_code == FitnessMachineStopCode.PAUSE:
@@ -58,10 +72,35 @@ class FitnessMachineDevice:
             return await self.stop()
 
     async def stop(self):
-        pass
+        return await self._send_ftms_command(
+            FitnessMachineControlPointOperation.STOP_OR_PAUSE,
+            FitnessMachineStopCode.STOP,
+        )
 
     async def pause(self):
-        pass
+        return await self._send_ftms_command(
+            FitnessMachineControlPointOperation.STOP_OR_PAUSE,
+            FitnessMachineStopCode.PAUSE,
+        )
 
     async def reset(self):
         pass
+
+    async def _send_ftms_command(
+        self,
+        operation: FitnessMachineControlPointOperation,
+        parameters=None,
+        response=True,
+    ):
+        await self.connect()
+
+        command = struct.pack(">B", operation)
+        if parameters:
+            command_parameters = struct.pack(">B", parameters)
+            command += command_parameters
+
+        return await self._client.write_gatt_char(
+            FTMS_FITNESS_MACHINE_CONTROL_POINT_CHARACTERISTIC_UUID,
+            command,
+            response=response,
+        )
